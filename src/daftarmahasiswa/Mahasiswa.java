54bcc24a2d6dfa43e0f57b7ef3b3f6d902a3cbc8
Muhammad Rafi Shidiq <133903731+slvally@users.noreply.github.com>
2023-05-25 08:23:09 +0700

    Add files via upload

---
+++ src/daftarmahasiswa/Mahasiswa.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package daftarmahasiswa;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Mahasiswa {
+    private String nim;
+    private String nama;
+    private String nilai;
+    private String ket;
+    
+    public Mahasiswa(String n1, String n2, String n3, String n4){
+        nim = n1;
+        nama = n2;
+        nilai = n3;
+        ket = n4;
+    }
+    
+    public void setNim(String n) {
+        nim = n;
+    }
+    
+    public void setNama(String n) {
+        nama = n;
+    }
+    
+    public void setNilai(String n) {
+        nilai = n;
+    }
+    
+    public void setKet(String n) {
+        ket = n;
+    }
+    
+    public String getNim() {
+        return nim;
+    }
+    
+    public String getNama() {
+        return nama;
+    }
+    
+    public String getNilai() {
+        return nilai;
+    }
+    
+    public String getKet() {
+        return ket;
+    }
+    
+    
+}
